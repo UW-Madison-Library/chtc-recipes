6df3859446612d0d740eeae52433d1ee732db27e
Steve Meyer <stephen.meyer@wisc.edu>
2020-02-17 10:32:24 -0600

    Update the find by export file sub-job to use new python and new WOS data

--- wos-findbyexport/findbyexportfile/find_by_wos_export.py
+++ wos-findbyexport/findbyexportfile/find_by_wos_export.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
-import pathlib
+import glob
+import os
 import csv
 from datetime import datetime
 from wos_explorer.article_collection import ArticleCollection
@@ -15,32 +16,34 @@ year       = sys.argv[1]
 cluster_id = sys.argv[2]
 process_id = sys.argv[3]
 
-input_file      = "data/articles.json"
+input_files     = glob.glob("data/*.json")
 exported_file   = "savedrecs.txt"
-output_file     = "article-matches-" + year + ".json"
 references_file = "references-" + year + ".tsv"
 
 
 print( "Find by WOS Export - Processing Data for Year:", year, cluster_id, process_id )
-
 finder_logging.configure(cluster_id, process_id)
 
 
+match_collections = []
 ids = [article['UT'] for article in csv.DictReader( open(exported_file), delimiter='\t' )]
 criteria = IdMatcher(ids)
-matches  = ArticleCollection(input_file).select(criteria, output_file)
+for input_file in input_files:
+    output_file = os.path.splitext(os.path.basename(input_file))[0] + "-article-matches.json"
+    match_collections.append( ArticleCollection(input_file).select(criteria, output_file) )
 
 
 article_match_count = 0
 reference_count = 0
-with open(references_file, 'w') as file:
-    for article in matches:
-        article_match_count += 1
-        for reference in article.references():
-            reference_count += 1
-            year = reference['year'] if reference['year'] is not None else 'BLANK'
-            id   = reference['id']   if reference['id']   is not None else ''
-            file.write('\t'.join([year, id]) + '\n')
+for matched_collection in match_collections:
+    with open(references_file, 'a') as file:
+        for article in matched_collection:
+            article_match_count += 1
+            for reference in article.references():
+                reference_count += 1
+                year = reference['year'] if reference['year'] is not None else 'BLANK'
+                id   = reference['id']   if reference['id']   is not None else ''
+                file.write('\t'.join([year, id]) + '\n')
 
 
 print("Articles:  ", article_match_count)
--- wos-findbyexport/findbyexportfile/wos_findbywosexport.sh
+++ wos-findbyexport/findbyexportfile/wos_findbywosexport.sh
@@ -14,11 +14,16 @@ process_id="$3"
 
 # Create a directory for the article data file so it is not copied back to the submit server
 working_data_dir="data"
-source_file="/mnt/gluster/data2/clarivate_data/CORE_ANNUAL_1900-2017/${year}_CORE/articles.json"
+source_dir="/mnt/gluster/data2/clarivate_data/2018-full-extract"
 
-# Copy the source file to the working location
+# Copy the source files to the working location. The Emerging Science Citation Index (ESCI)
+# files only exist from 2005 onward.
 mkdir $working_data_dir
-cp $source_file $working_data_dir
+cp "${source_dir}/${year}_CORE.json.gz" $working_data_dir
+if [ $year -gt "2004" ]; then
+  cp "${source_dir}/${year}_ESCI.json.gz" ${working_data_dir}
+fi
+gunzip "${working_data_dir}/"*.gz
 
 # Run the Python code to process the file and find records
 python3 find_by_wos_export.py $year $cluster_id $process_id
